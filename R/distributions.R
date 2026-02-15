d31609e2d218449e33f313236cd081e15c1a7101
frederik <f_ziebell@web.de>
2026-02-15 22:30:09 +0100

    add sample boxplots

--- R/distributions.R
+++ R/distributions.R
@@ -1,4 +1,6 @@
-plot_sample_distributions <- function(se, se_assay = 1, n_steps = 100){
+#' Plot one ECDF per sample
+#' @export
+plot_sample_ecdfs <- function(se, se_assay = 1, n_steps = 100){
   mat <- assay(se, i = se_assay)
   
   min_val <- min(mat, na.rm = T)
@@ -13,6 +15,29 @@ plot_sample_distributions <- function(se, se_assay = 1, n_steps = 100){
   walk(colnames(mat), function(col){
     ecdf_fun = ecdf(mat[,col])
     plot <<- plot + geom_function(fun = ecdf_fun, alpha = .2)
+  })
+  
+  plot
+}
+
+#' Plot one boxplot per sample
+#' @export
+plot_sample_boxplots <- function(se, se_assay = 1, ...){
+  mat <- assay(se, i = se_assay)
+  
+  
+  plot <- ggplot(mapping = aes(x,y)) +
+    cowplot::theme_cowplot()
+  
+  walk(colnames(mat), function(col){
+    plot <<- plot + 
+      geom_boxplot(
+        data = data.frame(
+          x = col,
+          y = mat[,col]
+        ),
+        ...
+      )
   })
   
   plot
