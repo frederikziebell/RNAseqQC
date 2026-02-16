0b649a358c54bd45a1a2b5f5da8d5b58525626f8
Frederik Ziebell <f_ziebell@web.de>
2026-02-16 13:11:33 +0100

    Add plot_sample_ecdfs() and plot_sample_boxplots() (#3)
    * add sample distribution plot
    
    * add sample boxplots
    
    * add examples, add plotting by group

--- R/RNAseqQC-package.R
+++ R/RNAseqQC-package.R
@@ -7,17 +7,17 @@
 #' @importFrom dplyr arrange mutate group_by left_join filter rename select summarize pull ungroup bind_cols row_number
 #' @importFrom tibble as_tibble column_to_rownames tibble enframe
 #' @importFrom magrittr %>%
-#' @importFrom purrr map map_dbl map_dfr
+#' @importFrom purrr map map_dbl imap map_dfr walk
 #' @importFrom tidyr pivot_longer pivot_wider
-#' @importFrom stringr str_sort str_c str_detect
-#' @importFrom ggplot2 ggplot aes geom_point geom_line element_text element_line
+#' @importFrom stringr str_sort str_c str_detect str_replace_all
+#' @importFrom ggplot2 ggplot aes geom_boxplot geom_function geom_point geom_line element_text element_line
 #' labs theme rel geom_path theme_minimal geom_smooth
 #' scale_x_log10 scale_y_log10 scale_x_sqrt scale_y_sqrt scale_y_continuous scale_color_manual
 #' scale_color_viridis_c scale_color_viridis_d scale_color_gradient scale_shape_discrete
 #' coord_fixed guides guide_colorbar guide_legend geom_histogram geom_hline ylim geom_hline
 #' @importFrom SummarizedExperiment assay rowData colData
 #' @importFrom utils head
-#' @importFrom stats as.dist cor dist p.adjust prcomp
+#' @importFrom stats as.dist cor dist ecdf p.adjust prcomp
 #' @importFrom matrixStats rowMedians
 #' @importFrom BiocGenerics colnames
 ## usethis namespace: end
--- R/global_vars.R
+++ R/global_vars.R
@@ -5,6 +5,6 @@ utils::globalVariables(
     ".",".data", "baseMean","biotype","chr_start","chromosome","col_sum","colname","count", "detection_threshold","fraction_of_counts",
     "fraction_of_genes","gc_content","gene_biotype","gene_id","gene_mean","gene_name","gene_sd","log_count", "log2FoldChange",
     "number_of_genes","padj","PC_rank","pvalue", "rowData","rowname","sample_id","seqnames","significant","start","svalue",
-    "total_count","tx_id","var_exp","ylim"
+    "total_count","tx_id","var_exp","x","y","ylim"
   )
 )
